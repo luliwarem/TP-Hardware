dc72126954342d9f4c8e93e8d0003987de43debc
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-04 09:08:17 -0300

    first commit

---
+++ Contactos.js
@@ -0,0 +1,8 @@
+import {View} from "react-native"
+
+
+export default function Contactos(){
+    return(<View>
+
+        </View>)
+}
---
+++ NavBar.js
@@ -0,0 +1,98 @@
+import {
+    Text,
+    View,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    Pressable,
+  } from "react-native";
+  import { Ionicons } from "@expo/vector-icons";
+  import * as React from "react";
+  import { NavigationContainer } from "@react-navigation/native";
+  import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+  
+  // Screens
+  import NumeroEmergencia from "./NumeroEmergencia";
+  import Contactos from "./Contactos";
+  import Clima from "./Clima";
+  import QR from "./QR";
+  
+  //Screen names
+  const numeroEmergenciaName = "Emergencia";
+  const contactosName = "Contactos";
+  const climaName = "Clima";
+  const QRName = "QR";
+  
+  const Tab = createBottomTabNavigator();
+  
+  export default function NavBar() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName={numeroEmergenciaName}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let rn = route.name;
+              if (rn === numeroEmergenciaName) {
+                iconName = focused ? "call" : "call-outline";
+              } else if (rn === contactosName) {
+                iconName = focused ? "person" : "person-outline";
+              } else if (rn === climaName) {
+                iconName = focused ? "qr-code" : "qr-code-outline";
+              } else if (rn === QRName) {
+                iconName = focused ? "partly-sunny" : "partly-sunny-outline";
+              } 
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "blue",
+            inactiveTintColor: "gray",
+            labelStyle: { fontSize: 10 },
+            tabBarStyle: { padding: 10, height: 100 },
+          }}
+        >
+          <Tab.Screen name={numeroEmergenciaName} component={NumeroEmergencia} />
+          <Tab.Screen name={contactosName} component={Contactos} />
+          <Tab.Screen name={climaName} component={Clima} />
+          <Tab.Screen name={QRName} component={QR} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      width: 300,
+      height: "15%",
+    },
+    botoncito: {
+      width: "auto",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 20,
+      shadowOffset: {
+        width: 2,
+        height: 3,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      padding: 10,
+      fontFamily: "sans-serif",
+      marginBottom: 10,
+    },
+  
+    gradient: {
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+    },
+  });
